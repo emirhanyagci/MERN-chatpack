4d2da042b87dac5dff8ce1be1914b926d0de5cbb
Emirhan Yagci <emirhan.yacis@gmail.com>
2024-07-06 14:41:09 +0300

    client:text item updated and attach icon added

--- frontend/src/features/chat/ChatInput.tsx
+++ frontend/src/features/chat/ChatInput.tsx
@@ -1,14 +1,17 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { Smile, SendHorizontal } from "lucide-react";
+import { Smile, SendHorizontal, Paperclip } from "lucide-react";
 export default function ChatInput() {
   return (
     <div className="flex items-center gap-1 p-5">
-      <Button variant="ghost">
+      <Button variant="ghost" size="sm">
+        <Paperclip className="cursor-pointer text-primary" />
+      </Button>
+      <Button variant="ghost" size="sm">
         <Smile className="cursor-pointer text-primary" />
       </Button>
       <Input type="text" />
-      <Button variant="ghost">
+      <Button variant="ghost" size="sm">
         <SendHorizontal className="cursor-pointer text-primary" />
       </Button>
     </div>
--- frontend/src/features/chat/TextItem.tsx
+++ frontend/src/features/chat/TextItem.tsx
@@ -12,10 +12,13 @@ export default function TextItem({
       className={`flex items-start justify-end gap-3 ${standLeft ? "flex-row-reverse" : ""}`}
     >
       <div
-        className={`max-w-64 rounded-lg p-2 text-sm ${standLeft ? "bg-muted text-foreground" : "bg-primary text-muted"}`}
+        className={`flex max-w-64 flex-col rounded-lg p-2 text-sm ${standLeft ? "bg-muted text-foreground" : "bg-primary text-foreground"}`}
       >
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis aut
-        distinctio dolorem quis hic dolores laborum totam nisi soluta eum!
+        <span>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis aut
+          distinctio dolorem quis hic dolores laborum totam nisi soluta eum!
+        </span>
+        <span className="text-end">21:32</span>
       </div>
       <div className="h-9 w-9">
         {withImage && (
